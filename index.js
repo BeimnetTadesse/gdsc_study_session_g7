e93ec48166fcf7c11ce8b12754d9b7c32dc872ac
nina233 <140155579+nina233@users.noreply.github.com>
2023-11-27 21:15:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const height = window.prompt("Insert your height:");
+const weight = window.prompt("Insert your weight:");
+const BMI =  weight / ( height * height);
+window.alert("Your BMI is " + BMI);
